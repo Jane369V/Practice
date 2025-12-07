359cc14f79bd9ef2789eb1d113d10922ad592f6a
Jane369V <janestasroma@gmail.com>
2025-12-07 13:14:49 +0400

    обавила комментарий к публичным функциям

--- practice/app.go
+++ practice/app.go
@@ -1,9 +1,11 @@
 package practice
 
+//Add возвращает сумму двух чисел.
 func Add(a, b int) int {
 	return a + b
 }
 
+//Multiply возвращает произведение двух чисел
 func Multiply(a, b int) int {
 	return a * b
 }
